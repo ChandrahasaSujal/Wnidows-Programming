98f69f16327e361a576118046fbdaef6bef5abca
chanducodemonk <chanducodemonk@gmail.com>
2018-10-31 15:46:31 +0530

    Add project files.

---
+++ ControlsProject/UserControl1.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace ControlsProject
+{
+    partial class StopClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.mtbTimer = new System.Windows.Forms.MaskedTextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // mtbTimer
+            // 
+            this.mtbTimer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.mtbTimer.Location = new System.Drawing.Point(3, 3);
+            this.mtbTimer.Mask = "00:00";
+            this.mtbTimer.Name = "mtbTimer";
+            this.mtbTimer.ReadOnly = true;
+            this.mtbTimer.Size = new System.Drawing.Size(102, 31);
+            this.mtbTimer.TabIndex = 0;
+            this.mtbTimer.Text = "0000";
+            this.mtbTimer.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(12, 40);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(81, 29);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStop.Location = new System.Drawing.Point(12, 75);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(81, 29);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnReset.Location = new System.Drawing.Point(12, 110);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(81, 29);
+            this.btnReset.TabIndex = 3;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(12, 145);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(81, 29);
+            this.btnClose.TabIndex = 4;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 997;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // StopClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.mtbTimer);
+            this.Name = "StopClock";
+            this.Size = new System.Drawing.Size(107, 185);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MaskedTextBox mtbTimer;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ ControlsProject/UserControl1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControlsProject
+{
+    public delegate void CloseEventHandler();
+    public partial class StopClock: UserControl
+    {
+        int min, sec;
+        string strSec, strMin;
+        public event CloseEventHandler CloseClick;
+        public StopClock()
+        {
+            InitializeComponent();
+        }
+
+        public void Start()
+        {
+            timer1.Start();
+        }
+        public void Stop()
+        {
+            timer1.Stop();
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+        }
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (sec < 59)
+            {
+                sec += 1;
+            }
+            else
+            {
+                sec = 0;
+                if (min < 59)
+                {
+                    min += 1;
+                }
+                else
+                {
+                    min = 0;
+                    timer1.Stop();
+                }
+            }
+            if(sec<10)
+            {
+                strSec = 0 + sec.ToString();
+            }
+            else
+            {
+                strSec = sec.ToString();
+            }
+            if(min<10)
+            {
+                strMin = 0 + min.ToString();
+            }
+            else
+            {
+                strMin = min.ToString();
+            }
+            mtbTimer.Text = strMin + strSec;
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            CloseClick();
+
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            mtbTimer.Text = "0000";
+            timer1.Stop();
+            min = sec = 0;
+        }
+        public string ElapsedTime
+        {
+            get { return mtbTimer.ToString(); }
+        }
+    }
+}
